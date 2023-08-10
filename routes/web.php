5dbaf90d06445296a1cecfdb7482153fe6005516
nemanja1991 <nemanja.jovanovic91@live.com>
2023-08-10 17:31:31 +0200

    books navigation bar

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('books');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/books', function () {
+    return view('books');
+})->middleware(['auth', 'verified'])->name('books');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
